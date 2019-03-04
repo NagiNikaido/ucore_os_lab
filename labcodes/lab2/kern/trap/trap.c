d33fbd24e751f023599c629b82aac02586b0bf67
NagiNikaido <naginikaido@gmail.com>
2019-03-04 14:58:50 +0800

    Lab2: patch applied.

--- labcodes/lab2/kern/trap/trap.c
+++ labcodes/lab2/kern/trap/trap.c
@@ -31,6 +31,7 @@ static struct pseudodesc idt_pd = {
     sizeof(idt) - 1, (uintptr_t)idt
 };
 
+
 /* idt_init - initialize IDT to each of the entry points in kern/trap/vectors.S */
 void
 idt_init(void) {
@@ -46,6 +47,15 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
+    
+    extern uintptr_t __vectors[]; 
+    int32_t i;
+
+    for (i = 0; i < sizeof(idt) / sizeof(struct gatedesc); i++) {
+        SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
+    }
+
+    lidt(&idt_pd);
 }
 
 static const char *
@@ -138,6 +148,7 @@ print_regs(struct pushregs *regs) {
 static void
 trap_dispatch(struct trapframe *tf) {
     char c;
+    static uint32_t tick_cnt = 0;
 
     switch (tf->tf_trapno) {
     case IRQ_OFFSET + IRQ_TIMER:
@@ -147,6 +158,10 @@ trap_dispatch(struct trapframe *tf) {
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
          * (3) Too Simple? Yes, I think so!
          */
+        if(++tick_cnt == TICK_NUM) {
+            tick_cnt = 0;
+            print_ticks();
+        }
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
