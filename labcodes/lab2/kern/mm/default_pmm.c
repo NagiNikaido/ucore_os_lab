79b79098fabc01280fc9dfd6221cb5256a60387d
NagiNikaido <naginikaido@gmail.com>
2019-03-25 20:35:28 +0800

    Lab 2 bug fixed.

--- labcodes/lab2/kern/mm/default_pmm.c
+++ labcodes/lab2/kern/mm/default_pmm.c
@@ -137,6 +137,7 @@ default_alloc_pages(size_t n) {
     if (page != NULL) {
         if (page->property > n) {
             struct Page *p = page + n;
+            SetPageProperty(p);
             p->property = page->property - n;
             list_add_after(&(page->page_link), &(p->page_link));
         }
